590d9012dd8fafdf12c7f5587aeb9d41a66ca2b1
Fabio Zammit <fabio@rootcodex.com>
2023-07-22 00:13:40 +0200

    changes to endpoints index

--- frontend/connectr/app/routes/endpoints._index.tsx
+++ frontend/connectr/app/routes/endpoints._index.tsx
@@ -2,7 +2,7 @@ import type { V2_MetaFunction } from "@remix-run/node";
 import { Title } from "~/components/common/Title";
 import { useNavigate } from "@remix-run/react";
 import { AppRouting } from "~/utils/routes";
-import { classNames } from "~/utils/common";
+import { classNames, getColours, getRandomArbitrary } from "~/utils/common";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -19,13 +19,20 @@ export default function EndpointsIndex() {
     Error: "text-rose-400 bg-rose-400/10",
   };
 
-  const people = [
+  const colours = getColours();
+
+  const endpoints = [
     {
       id: 1,
       name: "Lindsay Walton",
       title: "Front-end Developer",
-      email: "lindsay.walton@example.com",
-      role: "Member",
+      sources: [
+        "Ethereum",
+        "CoinGecko API",
+        "Ethereum",
+        "CoinGecko API",
+        "Ethereum",
+      ],
       status: "Running",
       date: "25/04/2023",
     },
@@ -56,6 +63,12 @@ export default function EndpointsIndex() {
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
+                    Name
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
                     Date &amp; Time
                   </th>
                   <th
@@ -79,44 +92,59 @@ export default function EndpointsIndex() {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
-                {people.map((person) => (
-                  <tr key={person.email} className="even:bg-gray-50">
+                {endpoints.map((endpoint) => (
+                  <tr key={endpoint.id} className="even:bg-gray-50">
                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-                      {person.name}
+                      #{endpoint.id}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                      {person.title}
+                      {endpoint.name}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                        {person.email}
-                      </span>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {endpoint.date}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 flex flex-wrap max-w-[285px]">
+                      {endpoint.sources.map((source, index) => (
+                        <span
+                          key={index}
+                          className={classNames(
+                            colours[index]
+                              ? colours[index]
+                              : colours[getRandomArbitrary(0, colours.length)],
+                            "mr-1 mb-1 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ring-green-600/20"
+                          )}
+                        >
+                          {source}
+                        </span>
+                      ))}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       <div className="flex items-center justify-end gap-x-2 sm:justify-start">
                         <time
                           className="text-gray-400 sm:hidden"
-                          dateTime={person.date}
+                          dateTime={endpoint.date}
                         >
-                          {person.date}
+                          {endpoint.date}
                         </time>
                         <div
                           className={classNames(
-                            statuses[person.status],
+                            statuses[endpoint.status],
                             "flex-none rounded-full p-1"
                           )}
                         >
                           <div className="h-1.5 w-1.5 rounded-full bg-current" />
                         </div>
                         <div className="hidden text-gray-500 sm:block">
-                          {person.status}
+                          {endpoint.status}
                         </div>
                       </div>
                     </td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-sm font-medium sm:pr-3">
                       <button
                         onClick={() =>
-                          navigate(AppRouting.CREATE_ACTIONS + "/" + person.id)
+                          navigate(
+                            AppRouting.CREATE_ACTIONS + "/" + endpoint.id
+                          )
                         }
                         type="button"
                         className="inline-flex items-center rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
--- frontend/connectr/app/utils/common.ts
+++ frontend/connectr/app/utils/common.ts
@@ -1,3 +1,18 @@
 export function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
+
+export function getColours() {
+  return [
+    ['bg-green-50 text-green-700'],
+    ['bg-red-50 text-red-700'],
+    ['bg-yellow-50 text-yellow-700'],
+    ['bg-blue-50 text-blue-700'],
+    ['bg-orange-50 text-orange-700'],
+    ['bg-rose-50 text-rose-700'],
+  ]
+}
+
+export function getRandomArbitrary(min : number, max : number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
