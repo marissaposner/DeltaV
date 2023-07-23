7d96896757cd5b3ae35307b187fbeb0856989b4e
Fabio Zammit <fabio@rootcodex.com>
2023-07-23 07:40:29 +0200

    adding screen to view payload data returned

--- frontend/connectr/app/routes/endpoints.view.$id.tsx
+++ frontend/connectr/app/routes/endpoints.view.$id.tsx
@@ -1,4 +1,4 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import { redirect, type V2_MetaFunction } from "@remix-run/node";
 import { Title } from "~/components/common/Title";
 import { useLoaderData } from "@remix-run/react";
 import { requireAuth } from "~/services/auth.server";
@@ -11,14 +11,20 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
-export const loader = async ({ request }) => {
+export const loader = async ({ request, params }) => {
   await requireAuth({ request });
 
-  return null;
+  const id = params.id;
+
+  if (!id) return redirect("/");
+
+  return {
+    id,
+  };
 };
 
 export default function EndpointsView() {
-  // const { endpoints } = useLoaderData();
+  const { id } = useLoaderData();
 
   const value = JSON.stringify([
     {
@@ -55,9 +61,10 @@ export default function EndpointsView() {
       <Title title="Endpoint Data" className="mb-9" />
 
       <div className="bg-white shadow-standard px-12 py-9">
-        <h2 className="text-base font-semibold leading-7 text-gray-900 mb-4">
+        <h2 className="text-base font-semibold leading-7 text-gray-900">
           Action Payload Data
         </h2>
+        <p className="mt-1 text-sm leading-6 text-gray-600 mb-4">ID #{id}</p>
         <InputTextArea
           name="payload"
           placeholder="Payload data"
